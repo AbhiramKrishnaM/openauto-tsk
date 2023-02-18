b3c9755b5b526c37ec9b427a506761b1f3641a7a
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-02-18 15:22:34 +0530

    added content component

--- src/components/base/Content.jsx
+++ src/components/base/Content.jsx
@@ -1,5 +1,10 @@
-function Content({ title, subtitle }) {
-  return <div></div>;
+function Content({ content }) {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold mb-5 text-light ">{content.title}</h1>
+      <span className="text-grey text-xl">{content.subtitle}</span>
+    </div>
+  );
 }
 
 export default Content;
--- src/components/sections/Info.jsx
+++ src/components/sections/Info.jsx
@@ -1,16 +1,28 @@
-import Footer from "../core/Footer";
+import Footer from "@/components/core/Footer";
+import Content from "@/components/base/Content";
+
 import OrderGuy from "@/assets/images/Order-ride.svg";
 
 function Info() {
+  const content = {
+    title: "Focused on Time Saving",
+    subtitle:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolore voluptate quos dicta quia corrupti consequuntur aut nostrum ullam, iusto magnam sunt temporibus deleniti veniam voluptas eum commodi modi ad aperiam voluptatem? Dolorum, aut sint? Exercitationem maxime dicta dolorem esse nemo corporis soluta ullam beatae dignissimos. Quae excepturi nihil earum!",
+  };
+
   return (
     <div>
-      <div className="flex items-center">
-        <div className="w-1/2">
+      <div className="flex items-center gap-14">
+        <div className="w-1/2 ">
           <img
             src={OrderGuy}
             alt="Guy waiting for a car"
-            className="h-[600px]"
+            className=" w-full h-full"
           />
+        </div>
+
+        <div className="w-3/6">
+          <Content content={content} />
         </div>
       </div>
       <Footer />
