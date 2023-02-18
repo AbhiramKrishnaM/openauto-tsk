8a79dbded6c7262592a7662f8690018dd3d9b3fc
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-02-18 17:24:42 +0530

    added icon

--- src/components/core/Footer.jsx
+++ src/components/core/Footer.jsx
@@ -1,13 +1,16 @@
 import Social from "@/components/core/Sociallinks";
 import Contact from "@/components/core/Contact";
 
+import Logo from "@/assets/images/logo.svg";
+
 function Footer() {
   return (
     <footer className="w-full">
       <div className="border-t-4 border-gray-800 py-10 flex items-center justify-between">
         {/* icon and rights */}
         <div className="w-1/6">
-          <div id="logo" className="h-14 bg-red-600 mb-2"></div>
+          <img id="logo" src={Logo} className="h-14  mb-2 cursor-pointer" />
+
           <span className="text-xl text-light">
             Open Auto @ all rights reserved
           </span>
--- src/components/core/Navbar.jsx
+++ src/components/core/Navbar.jsx
@@ -1,12 +1,14 @@
 import Contact from "@/components/core/Contact";
 import Button from "@/components/base/Button";
 
+import Logo from "@/assets/images/logo.svg";
+
 function Navbar() {
   return (
     <div className="flex items-center justify-between my-8">
-      <div id="logo" className="h-14 w-1/6 bg-red-600 mb-2"></div>
+      <img id="logo" src={Logo} className="h-14 w-1/6 mb-2 cursor-pointer" />
 
-      <div className="flex items-center gap-7">
+      <div className="flex items-center gap-10">
         <Contact />
         <Button title="Download the mobile app" />
       </div>
