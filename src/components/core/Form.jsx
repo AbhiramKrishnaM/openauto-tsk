a8df75268cf23a793ed62d16892ba797133b3577
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-02-18 13:15:52 +0530

    added components, for landing, info, footer

---
+++ src/components/core/Form.jsx
@@ -0,0 +1,7 @@
+import Button from "@components/base/Button";
+
+function Form() {
+  return <Button />;
+}
+
+export default Form;
---
+++ src/components/sections/Landing.jsx
@@ -0,0 +1,5 @@
+function Landing() {
+  return <div>hello</div>;
+}
+
+export default Landing;
