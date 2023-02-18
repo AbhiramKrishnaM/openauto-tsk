6a762ca10b10ac8969b1445de71cbdc56022507f
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-02-18 13:11:24 +0530

    added core components added props for loading, added input field components

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "@/components/base/Button";
+
+function App() {
+  return (
+    <div class="w-full h-screen flex items-center justify-center bg-primary">
+      <Button isLoading={true} />
+    </div>
+  );
+}
+
+export default App;
