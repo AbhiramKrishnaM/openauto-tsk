124a6cad95df4ff47854f319c4f598bdc33d70b5
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-02-19 23:34:54 +0530

    added navbar responsive

--- src/components/core/Navbar.jsx
+++ src/components/core/Navbar.jsx
@@ -5,15 +5,15 @@ import Logo from "@/assets/images/Logo.svg";
 
 function Navbar() {
   return (
-    <div className="flex items-center justify-between my-6">
+    <div className="flex   items-center lg:justify-between my-6">
       <img
         id="logo"
         src={Logo}
         alt="Open auto logo"
-        className="h-14 w-1/6 mb-2 cursor-pointer"
+        className="h-14  w-1/2 lg:w-1/3 xl:w-1/6 mb-2 cursor-pointer"
       />
 
-      <div className="flex items-center gap-10">
+      <div className="hidden xl:flex items-center gap-10">
         <Contact />
         <Button title="Download the mobile app" />
       </div>
